8f59b77d9aac8bb39a1391cd059ca60c19b99a98
anmol18102 <sachinanmol73@gmail.com>
2024-08-16 11:40:45 +0530

    first commmit

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className='flex justify-center items-center text-white text-3xl h-full'>
+      <div className='pt-72'>
+      Welcome to StudyNotion!!
+      </div>
+     
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='flex justify-center items-center text-white text-3xl h-full'>
+          <div className='pt-72'>
+              Home
+
+            </div> 
+       
+    </div>
+  )
+}
+
+export default Home
